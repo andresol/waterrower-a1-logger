67ea04f2bb01e1390caada583b7c41d686b8c803
Andre Sollie <andre@sollie.info>
2018-03-27 10:43:37 +0200

    Print env. Port is now a propertie.

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var express = require('express')
     , app = express(),
  compression = require('compression');
 
+const PORT = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || '';
+
 app.use(compression());
 app.use(express.static(__dirname + '/public'));
 app.use(require('./controllers'));
 
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000...')
+app.listen(PORT, function() {
+    console.log("Environment is " + env);
+    console.log('Listening on port ' + PORT)
 });
